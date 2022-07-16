d6bd358623f8681de0c693819caed18fe4cbf51b
git <karnavgamit@gmail.com>
2022-07-16 10:52:44 +0530

    Version 4

--- strCalc.cpp
+++ strCalc.cpp
@@ -8,26 +8,68 @@ int Add(string numbers)
         return 0;
     else
     {
-        if (numbers.find(",") == -1)
+        if (numbers.find(",") == -1 && numbers.find(";") == -1){
+            if(numbers[0] == '/' && numbers[1] == '/'){
+                string remstr = numbers.substr(4);
+                return (stoi(numbers));
+            }
             return (stoi(numbers));
+        }
 
+        if ((numbers[0] == '/') && (numbers[1] == '/'))
+        {
+            char delimeter = numbers[2];
+            int s = 0;
+            string temp1 = "";
+            vector<string> vctr;
+    
+            for (int i = 4; i < strlen; i++)
+            {
+                if ((i + 1) < strlen && numbers[i] == delimeter && numbers[i + 1] == '\n')
+                {
+                    cout << "Invalid input";
+                    exit(0);
+                }
+                if (numbers[i] == '\n' && temp1 != "")
+                {
+                    vctr.push_back(temp1);
+                    continue;
+                }
+
+                if (numbers[i] == delimeter)
+                {
+                    vctr.push_back(temp1);
+                    temp1 = "";
+                    continue;
+                }
+                if(numbers[i] >= '0' && numbers[i] <= '9')
+                    temp1 += numbers[i];
+            }
+            if (temp1 != "")
+                vctr.push_back(temp1);
+
+            for (auto n : vctr)
+                s += stoi(n);
+            
+            return s;
+        }
         else
         {
             int sum = 0;
             vector<string> str;
             string temp = "";
-            
+
             for (int i = 0; i < strlen; i++)
             {
-                if((i+1) < strlen && numbers[i] == ',' && numbers[i+1] == '\n')
+                if ((i + 1) < strlen && numbers[i] == ',' && numbers[i + 1] == '\n')
                 {
                     cout << "Invalid input";
                     exit(0);
                 }
-                if(numbers[i] == '\n' && temp!="")
+                if (numbers[i] == '\n' && temp != "")
                 {
                     str.push_back(temp);
-                    temp="";
+                    temp = "";
                     continue;
                 }
                 if (numbers[i] == ',')
@@ -36,6 +78,7 @@ int Add(string numbers)
                     temp = "";
                     continue;
                 }
+                if(numbers[i]>='0' && numbers[i]<='9')
                 temp += numbers[i];
             }
             if (temp != "")
@@ -51,6 +94,6 @@ int Add(string numbers)
 
 int main()
 {
-    cout << Add("1\n2,3");
+    cout << Add("1");
     return 0;
 }
